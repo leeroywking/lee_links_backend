46ecf00ea95c52c0b8367f02b4b08ccae36def30
Lee-Roy King <leeroywking@gmail.com>
2021-10-23 11:09:20 -0700

    working but with caching issue

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 load_dotenv("./.env")
 
-# SPOTIFY_CLIENTID = os.environ.get("SPOTIFY_CLIENTID")
-# SPOTIFY_SECRET = os.environ.get("SPOTIFY_SECRET")
+SPOTIFY_CLIENTID = os.environ.get("SPOTIFY_CLIENTID")
+SPOTIFY_SECRET = os.environ.get("SPOTIFY_SECRET")
 # print(SPOTIFY_CLIENTID, SPOTIFY_SECRET)
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
 
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=SPOTIFY_CLIENTID,
+                                                           client_secret=SPOTIFY_SECRET))
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENTID,
+                                               client_secret=SPOTIFY_SECRET,
+                                               redirect_uri="https://localhost.com",
+                                               scope="user-library-read"))
+
+# results = sp.search(q='weezer', limit=20)
+# for idx, track in enumerate(results['tracks']['items']):
+#     print(idx, track['name'])
+
+data = sp.current_user_playing_track()
+print(data)
